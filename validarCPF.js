ac78299a80e8252f26e719c2db84d1833cf8782d
lloliveirax <83791944+lloliveirax@users.noreply.github.com>
2022-11-01 14:33:37 -0300

    ajustes

--- validarCPF.js
+++ validarCPF.js
@@ -19,6 +19,21 @@ function validarCPF(CPF) {
         i < 9 ? digitosComuns.push(cpf[i]) : digitosValidadores.push(cpf[i]);
     };
 
+    /*Validação números iguais */
+    let valid = true;
+    for (let i = 0; i < cpf.length - 1; i++) {
+        const num = cpf[i];
+        const prox = cpf[i + 1];
+        if (num != prox) {
+            valid = true;
+            break;
+        } else {
+            valid = false;
+        };
+    };
+    if (!valid) return false;
+
+
     /*Validação 10º número*/
     let somaMultiplicacao = multiplicarValores(10, digitosComuns);
     const resto = (somaMultiplicacao * 10) % 11 === 10 ? 0 : (somaMultiplicacao * 10) % 11;
@@ -38,3 +53,5 @@ function validarCPF(CPF) {
 };
 
 export default validarCPF;
+console.log(validarCPF('440.176.740-38'));
+console.log(validarCPF('222.222.222-22'));
